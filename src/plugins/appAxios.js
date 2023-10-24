9d02c0b354a3a3b0dc3064405f7f113767651408
DuyenPhan <22424005@student.hcmus.edu.vn>
2023-10-24 07:15:25 +0700

    add refresh token when 401

--- src/plugins/appAxios.js
+++ src/plugins/appAxios.js
@@ -1,8 +1,26 @@
 import axios from 'axios';
+import isTokenExpired from '../utils/isTokenExpired'
 
 const $axios = axios
+let accessToken = localStorage.getItem("accessToken")?.replaceAll('"','') || ''
+if(accessToken && isTokenExpired(accessToken)) {
+  try {
+    const refreshToken = localStorage.getItem("refreshToken")?.replaceAll('"','')
+    const res = await axios.request("http://localhost:3000/api/auth/refresh", {
+      method: 'post',
+      data: { refreshToken },
+      headers: {'Authorization': `Bearer ${accessToken}`}
+    })
+    if(res.data.success && typeof res.data.data === 'string') {
+      localStorage.setItem("accessToken", res.data.data)
+      accessToken = res.data.data
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
 $axios.defaults.baseURL = 'http://localhost:3000/'
-$axios.defaults.headers.common = {'Authorization': `Bearer ${localStorage.getItem("accessToken")?.replaceAll('"','') || ''}`}
+$axios.defaults.headers.common = {'Authorization': `Bearer ${accessToken}`}
 
 export default {
   install: (app) => {
